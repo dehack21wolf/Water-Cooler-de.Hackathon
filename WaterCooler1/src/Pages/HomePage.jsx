a4d1d6537ba7943b69754528c5f2545886adc45b
Leevanni Goncalves <leevanni@Leevannis-MacBook-Pro.local>
2021-05-11 01:51:27 -0400

    modified:   src/Pages/HomePage.jsx

--- WaterCooler1/src/Pages/HomePage.jsx
+++ WaterCooler1/src/Pages/HomePage.jsx
@@ -1,22 +1,40 @@
 
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Department from "../components/Department"
 import "./HomePage.css";
 
 
 class HomePage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            departments: []
+        };
+
+    }
+
+    componentDidMount() {
+       
+    }
+    
+
     render() {
         return (
-            <div className="department-container">
-                <Department />
-                <Department />
+            <div className="homepage-container">
+                <div className="department-container">
+                    <Department />
+                    <Department />
+                    <Department />
+                    <Department />
+                    <Department />
+                    <Department />
+                    <Department />
+                </div>
 
-
+                <button className="admin-button" onClick={this.togglePopup}>Admin</button>
             </div>
-
-
-
-            
+   
         )
     }
 }
