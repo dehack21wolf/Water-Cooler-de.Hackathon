53ca8fa02d25d948f1a59bb4e432cfcb8380a05b
Alejandro Brito <alejandroalainbrito@gmail.com>
2021-05-13 00:19:38 -0400

    Finished the Employee component to display props which are passed to it. Added some comments as well Added lines to Department.js for sample data and changing the props which are passed

--- WaterCooler1/src/components/Employee.js
+++ WaterCooler1/src/components/Employee.js
@@ -1,14 +1,57 @@
 import React from 'react';
 import "./Employee.css"
+import ReactModal from 'react-modal';
 
-const Employee = (props) => {
+ReactModal.setAppElement('#root');
 
-
+class Employee extends React.Component {
+    // create an Employee component using the props we are passed.
+    // the props must have the 4 fields specified
+    constructor(props){
+      super(props);
+      this.state = {
+        showModal: false,
+        name: props.name,
+        email: props.email,
+        about: props.about,
+        image: props.image
+      };
     
-
-    return (
-        <img className="employee-avi" src="./employee.jpeg" className="rounded-circle" alt="Employee" width="50" height="50"/>
-    );
-}
+      // binding
+      this.handleOpenModal = this.handleOpenModal.bind(this);
+      this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
+    
+    handleOpenModal () {
+      this.setState({ showModal: true });
+    }
+    
+    handleCloseModal () {
+      this.setState({ showModal: false });
+    }
+  
+    render() {
+        return (
+          <span>
+          <img className="employee-avi" src={this.state.image} className="rounded-circle" alt="Employee" width="50" height="50" onClick={this.handleOpenModal} />
+          <ReactModal 
+             isOpen={this.state.showModal}
+             contentLabel="onRequestClose Example"
+             onRequestClose={this.handleCloseModal}
+             className="Modal"
+             overlayClassName="Overlay"
+          >
+            <button className="right" onClick={this.handleCloseModal}>X</button>
+            <div className="center">
+            <img className="employee-avi" src="./employee.jpeg" className="rounded-circle" alt="Employee" width="200" height="200" />
+            <p>Name: {this.state.name}</p>
+            <p>Email: {this.state.email} </p>
+            <p>About Me: {this.state.about}</p>
+            </div>
+          </ReactModal>
+          </span>
+        );
+    }
+  }
  
 export default Employee;
