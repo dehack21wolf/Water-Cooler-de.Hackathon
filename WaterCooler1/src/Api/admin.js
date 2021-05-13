4fe4a4ad8aba6bd9a415f1cdb88adb84eae595e1
natnaelkibe22 <natnael.kibe@gmail.com>
2021-05-13 13:00:44 -0400

    added department routes as second starter

---
+++ WaterCooler1/src/Api/admin.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000/',
+})
+
+export const postcredential = payload => api.post(`/admin/${id}`, payload)
+export const updatedepartment = payload => api.post(`/admin/updatedepartment`, payload)
+export const updateemployee = payload => api.post(`/admin/updateemployee`, payload)
+export const createdepartment = payload => api.post(`/admin/createdepartment`, payload)
+export const deletedepartment = id => api.delete(`/admin/book/${id}/delete`)
+
+
+const apis = {
+    postcredential,
+    updatedepartment,
+    updateemployee,
+    createdepartment,
+    deletedepartment,
+}
+
+export default apis
